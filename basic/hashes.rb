05836447645f93b8003acddf7661330fe1e8b4ce
Tazzmanian <banskaliqtabg@gmail.com>
2022-01-12 18:38:28 +0200

    Hashes

---
+++ basic/hashes.rb
@@ -0,0 +1,18 @@
+months = Hash.new
+months = Hash.new( "month" )
+months = Hash.new "month"
+puts "#{months[0]}"
+puts "#{months[72]}"
+
+H = Hash["a" => 100, "b" => 200]
+
+puts "#{H['a']}"
+puts "#{H['b']}"
+
+# methods
+# $, = ", "
+months = Hash.new( "month" )
+months = {"1" => "January", "2" => "February"}
+
+keys = months.keys
+puts "#{keys}"
